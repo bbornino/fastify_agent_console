f5cf3f233a0d443650f1e34dfb3084ec68c397dd
Brian Bornino <brianbornino@gmail.com>
2026-08-13 14:31:34 -0700

    Add JWT plugin with access token signing and authenticate decorator

---
+++ apps/api/src/app/plugins/jwt.ts
@@ -0,0 +1,18 @@
+import type { FastifyInstance } from 'fastify'
+import fastifyJwt from '@fastify/jwt'
+import fp from 'fastify-plugin'
+
+export default fp(async function (fastify: FastifyInstance) {
+    fastify.register(fastifyJwt, {
+        secret: process.env['JWT_ACCESS_SECRET'] as string,
+        sign: { expiresIn: '15m'},
+    })
+
+    fastify.decorate('authenticate', async function (request, reply) {
+        try {
+            await request.jwtVerify()
+        } catch (err) {
+            reply.code(401).send({ error: 'Unauthorized'})
+        }
+    })
+})
